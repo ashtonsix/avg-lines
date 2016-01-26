23463f1bac9c4f7bc30e6e9cd784265a470d52b1
Ashton War <me@ashtonwar.com>
2016-01-26 01:55:45 +0000

    Move -type option to prevent interfering w/ pipes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const i80 = arr => Math.ceil(arr.length * 0.8)
 const sum = arr => arr.reduce((pv, [v]) => pv + v, 0)
 
 export default args => new Promise((resolve, reject) =>
-  exec(`find ${args} -type f | xargs wc -l`, {maxBuffer: 1024 ** 2}, (err, input) => {
+  exec(`find -type f ${args} | xargs wc -l`, {maxBuffer: 1024 ** 2}, (err, input) => {
     if (err) reject(err)
     else {
       const files = input.split('\n').filter(v => v).map(l =>
         l.match(/^ *(\d+) +(.+)$/).slice(1)).
         map(([val, file]) => [parseInt(val, 10), file]).
         sort(([v0], [v1]) => v0 - v1).filter(([, filename]) => filename !== 'total')
-      if (!files.length) reject(`No files matched \`find ${args}\``)
+      if (!files.length) reject(`No files matched \`find -type f ${args}\``)
       else {
         resolve({
           numFiles: files.length,
